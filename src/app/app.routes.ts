f33f2ddc764b86979c80304d600718c2af387165
Delver Villanueva <xdelver_live@outlook.com>
2026-05-15 10:45:12 -0500

    config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,10 @@ import { Routes } from '@angular/router';
 import { DocumentReviewComponent } from './features/contracts/pages/document-review/document-review';
 
 export const routes: Routes = [
-  { path: '', component: DocumentReviewComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'contracts' },
+  {
+    path: 'contracts',
+    component: DocumentReviewComponent,
+    title: 'Document review',
+  },
 ];
